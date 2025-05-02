08ddcdc0a589ee24f747adb2633306e9358f1620
HansKindberg <hans.kindberg@gmail.com>
2025-05-02 11:54:47 +0200

    a

--- Tests/YamlDotNet-tests/Core/ParserTest.cs
+++ Tests/YamlDotNet-tests/Core/ParserTest.cs
@@ -15,7 +15,34 @@ namespace YamlDotNetTests.Core
 		#region Methods
 
 		[Theory]
-		[InlineData("Yaml-01.yml")]
+		[InlineData("Comments-01")]
+		public async Task Current_Iteration_Comments_Test(string fileName)
+		{
+			await Task.CompletedTask;
+
+			var text = await GetYaml(fileName);
+
+			var parsingEvents = new List<ParsingEvent>();
+
+			using(var stringReader = new StringReader(text))
+			{
+				var scanner = new Scanner(stringReader, false);
+				var parser = new Parser(scanner);
+
+				while(parser.MoveNext())
+				{
+					parsingEvents.Add(parser.Current!);
+				}
+			}
+
+			Assert.Equal(2, parsingEvents.Count);
+			Assert.True(parsingEvents[0] is StreamStart);
+			Assert.True(parsingEvents[1] is Comment);
+			Assert.Equal("First comment", ((Comment)parsingEvents[1]).Value);
+		}
+
+		[Theory]
+		[InlineData("Yaml-01")]
 		public async Task Current_Iteration_Test(string fileName)
 		{
 			await Task.CompletedTask;
@@ -43,7 +70,7 @@ namespace YamlDotNetTests.Core
 
 		private static async Task<string> GetYaml(string fileName)
 		{
-			return await GetYamlByPath(Path.Combine(_resourcesDirectory.FullName, fileName));
+			return await GetYamlByPath(Path.Combine(_resourcesDirectory.FullName, $"{fileName}.yml"));
 		}
 
 		private static async Task<string> GetYamlByPath(string path)
