8eebdb2c9c6d16b5091d77c0739e3c1b5c7e88ef
HansKindberg <hans.kindberg@gmail.com>
2025-05-24 11:24:45 +0200

    a

--- Source/Project/Yaml/YamlFormatter.cs
+++ Source/Project/Yaml/YamlFormatter.cs
@@ -1,30 +1,49 @@
 using HansKindberg.Text.Formatting.Yaml.Configuration;
+using HansKindberg.Text.Formatting.Yaml.Models;
 using HansKindberg.Text.Formatting.Yaml.Serialization;
-using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
 namespace HansKindberg.Text.Formatting.Yaml
 {
-	///// <inheritdoc />
-	public class YamlFormatter(IParser<IList<YamlNode>> parser)
+	/// <inheritdoc />
+	public class YamlFormatter(IParser<IYamlNode> parser) : ITextFormatter<YamlFormatOptions>
 	{
 		#region Properties
 
-		protected internal virtual string NewLine => Environment.NewLine;
-		protected internal virtual IParser<IList<YamlNode>> Parser { get; } = parser ?? throw new ArgumentNullException(nameof(parser));
-		protected internal virtual string StartOfDocument => "---";
+		protected internal virtual IParser<IYamlNode> Parser { get; } = parser ?? throw new ArgumentNullException(nameof(parser));
 
 		#endregion
 
 		#region Methods
 
-		protected internal virtual async Task<ISerializer> CreateSerializer(YamlFormatOptions options)
+		protected internal virtual async Task ApplyNamingConvention(IYamlNode node, YamlFormatOptions options)
 		{
-			return new SerializerBuilder()
-				.WithNamingConvention(await this.GetNamingConvention(options.NamingConvention))
-				//.WithNewLine(this.NewLine)
-				.Build();
+			if(node == null)
+				throw new ArgumentNullException(nameof(node));
+
+			if(options == null)
+				throw new ArgumentNullException(nameof(options));
+
+			if(options.NamingConvention == null)
+				return;
+
+			var namingConvention = await this.GetNamingConvention(options.NamingConvention);
+
+			if(namingConvention == null)
+				return;
+
+			await node.ApplyNamingConvention(namingConvention);
+		}
+
+		protected internal virtual async Task<IComparer<IYamlNode>> CreateYamlNodeComparer(YamlFormatOptions options)
+		{
+			if(options == null)
+				throw new ArgumentNullException(nameof(options));
+
+			await Task.CompletedTask;
+
+			return new YamlNodeComparer(options);
 		}
 
 		public virtual async Task<string> Format(YamlFormatOptions options, string text)
@@ -35,30 +54,26 @@ namespace HansKindberg.Text.Formatting.Yaml
 			if(text == null)
 				throw new ArgumentNullException(nameof(text));
 
-			var yamlNodes = await this.Parser.Parse(text);
+			var node = await this.Parser.Parse(text);
 
-			var result = string.Empty;
+			if(!node.Children.Any())
+				return string.Empty;
 
-			if(yamlNodes.Count <= 0)
-				return result;
+			await this.ApplyNamingConvention(node, options);
 
-			if(options.Sorting.Enabled)
-			{
-				//yamlDocument = await this.Sort(yamlDocument, options.Sorting);
-			}
+			await this.Sort(node, options);
 
-			var serializer = await this.CreateSerializer(options);
+			var value = await this.GetText(node, options);
 
-			var parts = yamlNodes.Select(serializer.Serialize).ToList();
-
-			result = (parts.Count == 1 ? parts[0] : parts.Aggregate(result, (current, part) => current + $"{this.StartOfDocument}{this.NewLine}{part}")).Trim();
-
-			return result;
+			return value;
 		}
 
-		protected internal virtual async Task<INamingConvention> GetNamingConvention(NamingConvention? namingConvention)
+		protected internal virtual async Task<INamingConvention?> GetNamingConvention(NamingConvention? namingConvention)
 		{
 			await Task.CompletedTask;
+
+			if(namingConvention == null)
+				return null;
 
 			return namingConvention switch
 			{
@@ -67,8 +82,39 @@ namespace HansKindberg.Text.Formatting.Yaml
 				NamingConvention.LowerCase => LowerCaseNamingConvention.Instance,
 				NamingConvention.PascalCase => PascalCaseNamingConvention.Instance,
 				NamingConvention.Underscored => UnderscoredNamingConvention.Instance,
-				_ => NullNamingConvention.Instance
+				_ => null
 			};
+		}
+
+		protected internal virtual async Task<string> GetText(IYamlNode node, YamlFormatOptions options)
+		{
+			if(node == null)
+				throw new ArgumentNullException(nameof(node));
+
+			if(options == null)
+				throw new ArgumentNullException(nameof(options));
+
+			var lines = new List<string>();
+
+			await node.Write(lines, options);
+
+			return string.Join(options.NewLine, lines).Trim();
+		}
+
+		protected internal virtual async Task Sort(IYamlNode node, YamlFormatOptions options)
+		{
+			if(node == null)
+				throw new ArgumentNullException(nameof(node));
+
+			if(options == null)
+				throw new ArgumentNullException(nameof(options));
+
+			if(!options.DocumentSorting.Enabled && !options.ScalarSorting.Enabled && !options.SequenceSorting.Enabled)
+				return;
+
+			var comparer = await this.CreateYamlNodeComparer(options);
+
+			await node.Sort(comparer);
 		}
 
 		#endregion
